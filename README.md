0a31b1eb87a48424407288f8cd73ebe61b60d2e3
Igor <11178422666>
2019-12-20 17:23:46 -0300

    Added long_description and updated README.md

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Vector IO - Utility for working with vector data
-
+# Vector IO - Geoprocessing utility for working with vector data.
 
 ## Requirements
-- python3.7 e pip3 (or pip3.7)
+- python3.7
 - gdal >= 2.2
+
+## Description
+This project is a tool for working with vectorial data based on [GDAL](https://gdal.org/). This tool is an envelope about gdal and aims to work with different types of vector data quickly, intelligently, and simply.  
+
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='vectorio',
-    version='1.0.0',
-    description='utility for working with vector data',
+    version='1.0.2',
+    description='Geoprocessing utility for working with vector data',
     author='Igor Rodrigues Sousa Silva',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='igor.rodrigues.ss10@gmail.com',
     url='https://github.com/igor-rodrigues-ss/vectorio',
     test_suite='tests',
