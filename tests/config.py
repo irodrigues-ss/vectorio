bea94bee4f3eada0ede71d5f436a94a2b6d3b5b5
Igor <11178422666>
2020-01-22 16:37:20 -0300

    Adjuted config file for tests

--- tests/config.py
+++ tests/config.py
@@ -1,18 +1,8 @@
 #!-*-coding:utf-8-*-
 
 import os
+from vectorio.config import ROOT_PROJECT
 
-
-def get_root(directory):
-    dirname = os.path.dirname(directory)
-    if 'setup.py' in os.listdir(dirname):
-        return dirname
-    else:
-        return get_root(dirname)
-
-
-ROOT_PROJECT = get_root(__file__)
-
-TESTS_DIRPATH = os.path.dirname(__file__)
+TESTS_DIRPATH = os.path.join(ROOT_PROJECT, 'tests')
 FIXTURES_DIRPATH = os.path.join(TESTS_DIRPATH, 'fixtures')
 FILESDIR_FROM_FIXTURES = os.path.join(FIXTURES_DIRPATH, 'files')
